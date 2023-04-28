eaab7a27f604825059eb64114814c8de649d8720
Justin Dority <justindority@gmail.com>
2023-04-28 17:13:50 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,128 @@
 import logo from './logo.svg';
 import './App.css';
 import axe from '../src/assets/portraits/npc_dota_hero_axe.webm';
+import abaddon from '../src/assets/portraits/npc_dota_hero_abaddon.webm';
+import underlord from '../src/assets/portraits/npc_dota_hero_abyssal_underlord.webm';
+import alchemist from '../src/assets/portraits/npc_dota_hero_alchemist.webm';
+import ancientapparition from '../src/assets/portraits/npc_dota_hero_ancient_apparition.webm';
+import antimage from '../src/assets/portraits/npc_dota_hero_antimage.webm';
+import arcwarden from '../src/assets/portraits/npc_dota_hero_arc_warden.webm';
+import armadillo from '../src/assets/portraits/npc_dota_hero_armadillo.webm';
+import bane from '../src/assets/portraits/npc_dota_hero_bane.webm';
+import batrider from '../src/assets/portraits/npc_dota_hero_batrider.webm';
+import beastmaster from '../src/assets/portraits/npc_dota_hero_beastmaster.webm';
+import bloodseeker from '../src/assets/portraits/npc_dota_hero_bloodseeker.webm';
+import bountyhunter from '../src/assets/portraits/npc_dota_hero_bounty_hunter.webm';
+import brewmaster from '../src/assets/portraits/npc_dota_hero_brewmaster.webm';
+import bristleback from '../src/assets/portraits/npc_dota_hero_bristleback.webm';
+import broodmother from '../src/assets/portraits/npc_dota_hero_broodmother.webm';
+import centaur from '../src/assets/portraits/npc_dota_hero_centaur.webm';
+import chaosknight from '../src/assets/portraits/npc_dota_hero_chaos_knight.webm';
+import chen from '../src/assets/portraits/npc_dota_hero_chen.webm';
+import clinkz from '../src/assets/portraits/npc_dota_hero_clinkz.webm';
+import crystalmaiden from '../src/assets/portraits/npc_dota_hero_crystal_maiden.webm';
+import darkseer from '../src/assets/portraits/npc_dota_hero_dark_seer.webm';
+import darkwillow from '../src/assets/portraits/npc_dota_hero_dark_willow.webm';
+import dawnbreaker from '../src/assets/portraits/npc_dota_hero_dawnbreaker.webm';
+import dazzle from '../src/assets/portraits/npc_dota_hero_dazzle.webm';
+import deathprophet from '../src/assets/portraits/npc_dota_hero_death_prophet.webm';
+import disruptor from '../src/assets/portraits/npc_dota_hero_disruptor.webm';
+import doom from '../src/assets/portraits/npc_dota_hero_doom_bringer.webm';
+import dragonknight from '../src/assets/portraits/npc_dota_hero_dragon_knight.webm';
+import drowranger from '../src/assets/portraits/npc_dota_hero_drow_ranger.webm';
+import earthspirit from '../src/assets/portraits/npc_dota_hero_earth_spirit.webm';
+import earthshaker from '../src/assets/portraits/npc_dota_hero_earthshaker.webm';
+import eldertitan from '../src/assets/portraits/npc_dota_hero_elder_titan.webm';
+import emberspirit from '../src/assets/portraits/npc_dota_hero_ember_spirit.webm';
+import enchantress from '../src/assets/portraits/npc_dota_hero_enchantress.webm';
+import enigma from '../src/assets/portraits/npc_dota_hero_enigma.webm';
+import facelessvoid from '../src/assets/portraits/npc_dota_hero_faceless_void.webm';
+import furion from '../src/assets/portraits/npc_dota_hero_furion.webm';
+import grimstroke from '../src/assets/portraits/npc_dota_hero_grimstroke.webm';
+import gyrocopter from '../src/assets/portraits/npc_dota_hero_gyrocopter.webm';
+import hoodwink from '../src/assets/portraits/npc_dota_hero_hoodwink.webm';
+import huskar from '../src/assets/portraits/npc_dota_hero_huskar.webm';
+import invoker from '../src/assets/portraits/npc_dota_hero_invoker.webm';
+import jakiro from '../src/assets/portraits/npc_dota_hero_jakiro.webm';
+import juggernaut from '../src/assets/portraits/npc_dota_hero_juggernaut.webm';
+import kotl from '../src/assets/portraits/npc_dota_hero_keeper_of_the_light.webm';
+import kunkka from '../src/assets/portraits/npc_dota_hero_kunkka.webm';
+import legion from '../src/assets/portraits/npc_dota_hero_legion_commander.webm';
+import leshrac from '../src/assets/portraits/npc_dota_hero_leshrac.webm';
+import lich from '../src/assets/portraits/npc_dota_hero_lich.webm';
+import lifestealer from '../src/assets/portraits/npc_dota_hero_life_stealer.webm';
+import lina from '../src/assets/portraits/npc_dota_hero_lina.webm';
+import lion from '../src/assets/portraits/npc_dota_hero_lion.webm';
+import lonedruid from '../src/assets/portraits/npc_dota_hero_lone_druid.webm';
+import luna from '../src/assets/portraits/npc_dota_hero_luna.webm';
+import lycan from '../src/assets/portraits/npc_dota_hero_lycan.webm';
+import magnus from '../src/assets/portraits/npc_dota_hero_magnataur.webm';
+import marci from '../src/assets/portraits/npc_dota_hero_marci.webm';
+import mars from '../src/assets/portraits/npc_dota_hero_mars.webm';
+import medusa from '../src/assets/portraits/npc_dota_hero_medusa.webm';
+import meepo from '../src/assets/portraits/npc_dota_hero_meepo.webm';
+import mirana from '../src/assets/portraits/npc_dota_hero_mirana.webm';
+import monkeyking from '../src/assets/portraits/npc_dota_hero_monkey_king.webm';
+import morphling from '../src/assets/portraits/npc_dota_hero_morphling.webm';
+import nagasiren from '../src/assets/portraits/npc_dota_hero_naga_siren.webm';
+import necrophos from '../src/assets/portraits/npc_dota_hero_necrolyte.webm';
+import nevermore from '../src/assets/portraits/npc_dota_hero_nevermore.webm';
+import nightstalker from '../src/assets/portraits/npc_dota_hero_night_stalker.webm';
+import nyxassassin from '../src/assets/portraits/npc_dota_hero_nyx_assassin.webm';
+import od from '../src/assets/portraits/npc_dota_hero_obsidian_destroyer.webm';
+import ogremagi from '../src/assets/portraits/npc_dota_hero_ogre_magi.webm';
+import omniknight from '../src/assets/portraits/npc_dota_hero_omniknight.webm';
+import oracle from '../src/assets/portraits/npc_dota_hero_oracle.webm';
+import pangolier from '../src/assets/portraits/npc_dota_hero_pangolier.webm';
+import phantomassassin from '../src/assets/portraits/npc_dota_hero_phantom_assassin.webm';
+import phantomlancer from '../src/assets/portraits/npc_dota_hero_phantom_lancer.webm';
+import phoenix from '../src/assets/portraits/npc_dota_hero_phoenix.webm';
+import primalbeast from '../src/assets/portraits/npc_dota_hero_primal_beast.webm';
+import puck from '../src/assets/portraits/npc_dota_hero_puck.webm';
+import pudge from '../src/assets/portraits/npc_dota_hero_pudge.webm';
+import pugna from '../src/assets/portraits/npc_dota_hero_pugna.webm';
+import queenofpain from '../src/assets/portraits/npc_dota_hero_queenofpain.webm';
+import clockwerk from '../src/assets/portraits/npc_dota_hero_rattletrap.webm';
+import razor from '../src/assets/portraits/npc_dota_hero_razor.webm';
+import riki from '../src/assets/portraits/npc_dota_hero_riki.webm';
+import rubick from '../src/assets/portraits/npc_dota_hero_rubick.webm';
+import sandking from '../src/assets/portraits/npc_dota_hero_sand_king.webm';
+import shadowdemon from '../src/assets/portraits/npc_dota_hero_shadow_demon.webm';
+import shadowshaman from '../src/assets/portraits/npc_dota_hero_shadow_shaman.webm';
+import silencer from '../src/assets/portraits/npc_dota_hero_silencer.webm';
+import skeleton_king from '../src/assets/portraits/npc_dota_hero_skeleton_king.webm';
+import skywrathmage from '../src/assets/portraits/npc_dota_hero_skywrath_mage.webm';
+import slardar from '../src/assets/portraits/npc_dota_hero_slardar.webm';
+import slark from '../src/assets/portraits/npc_dota_hero_slark.webm';
+import snapfire from '../src/assets/portraits/npc_dota_hero_snapfire.webm';
+import sniper from '../src/assets/portraits/npc_dota_hero_sniper.webm';
+import spectre from '../src/assets/portraits/npc_dota_hero_spectre.webm';
+import spiritbreaker from '../src/assets/portraits/npc_dota_hero_spirit_breaker.webm';
+import stormspirit from '../src/assets/portraits/npc_dota_hero_storm_spirit.webm';
+import sven from '../src/assets/portraits/npc_dota_hero_sven.webm';
+import techies from '../src/assets/portraits/npc_dota_hero_techies.webm';
+import templarassassin from '../src/assets/portraits/npc_dota_hero_templar_assassin.webm';
+import terrorblade from '../src/assets/portraits/npc_dota_hero_terrorblade.webm';
+import tidehunter from '../src/assets/portraits/npc_dota_hero_tidehunter.webm';
+import tinker from '../src/assets/portraits/npc_dota_hero_tinker.webm';
+import tiny from '../src/assets/portraits/npc_dota_hero_tiny.webm';
+import treant from '../src/assets/portraits/npc_dota_hero_treant.webm';
+import trollwarlord from '../src/assets/portraits/npc_dota_hero_troll_warlord.webm';
+import tusk from '../src/assets/portraits/npc_dota_hero_tusk.webm';
+import undying from '../src/assets/portraits/npc_dota_hero_undying.webm';
+import ursa from '../src/assets/portraits/npc_dota_hero_ursa.webm';
+import vengefulspirit from '../src/assets/portraits/npc_dota_hero_vengefulspirit.webm';
+import venomancer from '../src/assets/portraits/npc_dota_hero_venomancer.webm';
+import viper from '../src/assets/portraits/npc_dota_hero_viper.webm';
+import visage from '../src/assets/portraits/npc_dota_hero_visage.webm';
+import voidspirit from '../src/assets/portraits/npc_dota_hero_void_spirit.webm';
+import warlock from '../src/assets/portraits/npc_dota_hero_warlock.webm';
+import weaver from '../src/assets/portraits/npc_dota_hero_weaver.webm';
+import windrunner from '../src/assets/portraits/npc_dota_hero_windrunner.webm';
+import winterwyvern from '../src/assets/portraits/npc_dota_hero_winter_wyvern.webm';
+import wisp from '../src/assets/portraits/npc_dota_hero_wisp.webm';
+import witchdoctor from '../src/assets/portraits/npc_dota_hero_witch_doctor.webm';
+import zeus from '../src/assets/portraits/npc_dota_hero_zuus.webm'; 
 
 function App() {
   return (
@@ -21,6 +143,15 @@ function App() {
       </header>
       <video width="150rem" height="200rem">
         <source src={axe} type='video/webm'></source>
+      </video>
+      <video width="150rem" height="200rem">
+        <source src={witchdoctor} type='video/webm'></source>
+      </video>
+      <video width="150rem" height="200rem">
+        <source src={sven} type='video/webm'></source>
+      </video>
+      <video width="150rem" height="200rem">
+        <source src={riki} type='video/webm'></source>
       </video>
     </div>
   );
