ef12aa457fb920a7ab0ee9221b0f7889f129884d
Justin Dority <justindority@gmail.com>
2024-01-24 16:37:23 -0600

    new profile

--- src/components/randomprofiles/newprofile.js
+++ src/components/randomprofiles/newprofile.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import { getHeroes } from "../../managers/heroManager"
+import { createProfile } from "../../managers/profileManager"
 
 export const NewProfile = () => {
 
@@ -128,15 +129,17 @@ export const NewProfile = () => {
         121:true,
         122:true,
         123:true,
-        124:true,}
+        124:true,
+        "title": ""
+        }
     )
 
     useEffect(()=>{
         getHeroes().then(heroes => setHeroes(heroes))
     }, [])
 
-    const changeHandler = (e) => {
-        const copy = {...profileState}
+    const changeCheckedHandler = (e) => {
+        let copy = {...profileState}
         let id = e.target.id
         if (copy[id]){
             copy[id] = false
@@ -144,6 +147,17 @@ export const NewProfile = () => {
             copy[id] = true
         }
         setProfileState(copy)
+    }
+
+    const changeTitleHandler = (e) => {
+        let copy = {...profileState}
+        copy["title"] = e.target.value
+        setProfileState(copy)
+     }
+
+    const saveNewProfile = () => {
+        let copy = {...profileState}
+        createProfile(copy)
     }
 
 
@@ -155,13 +169,16 @@ export const NewProfile = () => {
                 return <>
                 { 
                     profileState[hero.id]
-                    ? <><input type="checkbox" id={hero?.id} onClick={(e)=>changeHandler(e)} value={hero?.name} checked/><label for={hero?.name}>{hero?.name}</label><br></br></>
-                    : <><input type="checkbox" id={hero?.id} onClick={(e)=>changeHandler(e)} value={hero?.name}/><label for={hero?.name}>{hero?.name}</label><br></br></>
+                    ? <><input type="checkbox" id={hero?.id} onClick={(e)=>changeCheckedHandler(e)} value={hero?.name} checked/><label for={hero?.name}>{hero?.name}</label><br></br></>
+                    : <><input type="checkbox" id={hero?.id} onClick={(e)=>changeCheckedHandler(e)} value={hero?.name}/><label for={hero?.name}>{hero?.name}</label><br></br></>
                 }</>
                 
             })
             : <></>
         }
+        <label for="title">Title</label>
+        <textarea value={profileState["title"]} onChange={(e)=>changeTitleHandler(e)} id="title"></textarea>
+        <button onClick={()=>saveNewProfile()}>Save</button>
         </>
     )
 
